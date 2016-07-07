c8c31572ec6df5836213b9713d441eae815996a4
Kairat <kayrat@bk.ru>
2016-07-07 16:08:19 +0600

    optimize sum, count, remove methods

--- app/async.js
+++ app/async.js
@@ -1,11 +1,21 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+    async: function (value) {
+        var promise = new Promise(function (resolve, reject) {
+                resolve(value);
+            }
+        );
+        return promise;
+    },
 
-  },
+    manipulateRemoteData: function (url) {
+        var data;
+        var promise = new Promise(function (resolve, reject) {
+            data = JSON.parse(url);
 
-  manipulateRemoteData: function(url) {
-
-  }
-};
+        });
+        alert(data.get('people')[0]);
+        return promise;
+    }
+}
