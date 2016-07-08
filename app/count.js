bb639755e16e6d21eb05cf5e4350fc6d0ca5a90a
Kairat <kayrat@bk.ru>
2016-07-08 15:47:21 +0600

    implement count.js functions

--- app/count.js
+++ app/count.js
@@ -1,7 +1,31 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-    count: function (start, end) {
 
+    count: function (start, end) {
+        var counter = {
+
+            stop: false,
+
+            promise: function () {
+                new Promise(function (resolve, reject) {
+                    var i = start;
+                    console.log(i);
+                    var interval = setInterval(function () {
+                        console.log(++i);
+                        if (i == end | stop) {
+                            clearInterval(interval);
+                        }
+                    }, 100);
+                });
+            },
+
+            cancel: function () {
+                stop = true;
+            }
+        };
+
+        counter.promise(start, end);
+        return counter;
     }
 };
