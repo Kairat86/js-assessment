1eb2660c207165db01f740ce35cb4e8690ec3844
Kairat <kayrat@bk.ru>
2016-07-12 16:10:43 +0600

    add objects.js

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,18 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
+    alterContext: function (fn, obj) {
+        return fn.call(obj);
+    },
 
-  },
+    alterObjects: function (constructor, greeting) {
+        constructor.prototype.greeting = greeting;
+    },
 
-  alterObjects: function(constructor, greeting) {
-
-  },
-
-  iterate: function(obj) {
-
-  }
-};
+    iterate: function (obj) {
+        return Object.keys(obj).map(function (item) {
+            return item + ': ' + obj[item];
+        })
+    }
+}
+;
