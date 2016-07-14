75f4bd2016c5bce8cb166da43241f13379471db7
Kairat <kayrat@bk.ru>
2016-07-14 11:56:37 +0600

    add regex.js

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,28 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
+    containsNumber: function (str) {
+        return /\d/.test(str);
+    },
 
-  },
+    containsRepeatingLetter: function (str) {
+        return (/([a-zA-Z]).*\1+/).test(str);
+    },
 
-  containsRepeatingLetter: function(str) {
+    endsWithVowel: function (str) {
+        return /[aeoiuAEIOU]$/.test(str);
+    },
 
-  },
+    captureThreeNumbers: function (str) {
+        var match = str.match(/\d{3}/);
+        return match === null ? false : match[0];
+    },
 
-  endsWithVowel: function(str) {
+    matchesPattern: function (str) {
+        return (/^\d{3}-\d{3}-\d{4}$/).test(str);
+    },
 
-  },
-
-  captureThreeNumbers: function(str) {
-
-  },
-
-  matchesPattern: function(str) {
-
-  },
-
-  isUSD: function(str) {
-
-  }
+    isUSD: function (str) {
+        return /^\$\d+(,\d{3})*(\.\d{2})?$/.test(str);
+    }
 };
